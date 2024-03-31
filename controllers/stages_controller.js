654c49a1ef430603e4f45d61ad8ab6b5a9f07f96
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2024-03-31 00:34:58 -0700

    part 11: created events_controller, stage_controller and respective routes

---
+++ controllers/stages_controller.js
@@ -0,0 +1,80 @@
+// CONST DEPENDENCIES
+const stages = require('express').Router()
+const db = require('../models')
+const { Stage } = db
+const { Op } = require('sequelize')
+
+// FIND A STAGE
+stages.get('/', async (req, res) => {
+    try {
+        const foundStages = await Stage.findAll({
+            order: [ [ 'stage_name', 'ASC' ]],
+            where: {
+                stage_name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+            }
+        })
+        res.status(200).json(foundStages)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// FIND A SPECIFIC STAGE
+stages.get('/:id', async (req, res) => {
+    try {
+        const foundStage = await Stage.findOne({
+            where: { stage_id: req.params.id }
+        })
+        res.status(200).json(foundStage)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// CREATE A STAGE
+stages.post('/', async (req, res) => {
+    try {
+        const newStage = await Stage.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new stage',
+            data: newStage
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// UPDATE A STAGE
+stages.put('/:id', async (req, res) => {
+    try {
+        const updatedStages = await Stage.update(req.body, {
+            where: {
+                stage_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedStages} stage(s)`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE A STAGE
+stages.delete('/:id', async (req, res) => {
+    try {
+        const deletedStages = await Stage.destroy({
+            where: {
+                stage_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedStages} stage(s)`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// EXPORT
+module.exports = stages
